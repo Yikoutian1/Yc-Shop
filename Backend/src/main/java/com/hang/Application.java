bd291205b3f6619dafdf1be00a9e8bb885834623
Yikoutian1 <599882460@qq.com>
2023-08-15 19:42:58 +0800

    初始化仓库

---
+++ Backend/src/main/java/com/hang/Application.java
@@ -0,0 +1,18 @@
+package com.hang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName Application
+ * @Description TODO
+ * @Author QiuLiHang
+ * @DATE 2023/8/15 19:36
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
